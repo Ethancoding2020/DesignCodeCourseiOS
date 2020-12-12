1c167c970209c551a18b48f9bf321043fae59efb
Ethan L <coding.ethan@gmail.com>
2020-12-12 17:59:32 -0500

    Initial Commit

---
+++ Shared/DesignCodeCourse_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourse_iOSApp.swift
+//  Shared
+//
+//  Created by Ethan on 2020-12-12.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourse_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
